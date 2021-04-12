a102f53fedae20cd1b0577eaae3dbe90f320ad86
MathiasReichardt <reichardt@bluehands.de>
2021-04-12 22:37:06 +0200

    Add script to build with ready docker image

--- generate-with-famboo-docker.ps1
+++ generate-with-famboo-docker.ps1
@@ -1,1 +1,1 @@
-docker run --rm -it -w /build -p 8085:8085 -v ${PWD}:/build  daux/daux.io daux generate
+docker run --rm -it -w /build -p 8085:8085 -v ${PWD}:/build  reimat/daux-famboo daux generate
