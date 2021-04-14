89e2811a42bb7c57f748f9a56a0c4eca0bd9da65
MathiasReichardt <reichardt@bluehands.de>
2021-04-14 13:37:13 +0200

    Update publish script: also publish latest

--- publish.ps1
+++ publish.ps1
@@ -1,6 +1,9 @@
 $tag = "1.1"
-
 docker build . -t reimat/daux-famboo:$tag
 if ($?) {
     docker push reimat/daux-famboo:$tag
+    
+    # also update latest
+    docker tag reimat/daux-famboo:$tag reimat/daux-famboo:latest
+    docker push reimat/daux-famboo:latest
 }
